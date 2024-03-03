81ad29aabdfb21fbf647156f1f6e7078b2599ed2
LamBust <olegoodno@gmail.com>
2024-03-03 19:29:35 +0300

    final 1.0 you can battle

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ racquet_right_x =  win_width - racquet_distance
 racquet_speed = 5
 
 ball_side = 50
-ball_speed = 2.5
+ball_speed = 7.5
 
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load('desk.jpg'), (win_width, win_height))
@@ -58,7 +58,8 @@ class Ball(GameSprite):
         self.up = up
         self.left = left
         self.down = down
-    def update(self):
+    def update(self, width, height):
+        global end
         if self.right:
             self.rect.x += self.speed
         if self.up:
@@ -67,19 +68,23 @@ class Ball(GameSprite):
             self.rect.y += self.speed
         if self.left:
             self.rect.x -= self.speed
-        if self.rect.y >= (win_height - self.height):
+        if self.rect.y >= (height - self.height):
             self.down = False
             self.up = True
         if self.rect.y <= 0:
             self.up = False
             self.down = True
-        if self.rect.x >= (win_width - self.width):
-            self.right = False
-            self.left = True
         if self.rect.x <= 0:
-            self.left = False    
-            self.right = True
-
+            end = True
+        if self.rect.x >= (width - self.width):
+            end = True
+    def touch_racquet(self):
+        if ball.right == True:
+                ball.right = False
+                ball.left = True
+        else:
+            ball.right = True
+            ball.left = False
 ball = Ball('ball.png', random.randint(400, 600), random.randint(10, (win_height - ball_side - 10)), ball_speed, ball_side, ball_side, right, up, left, down)
 
 rockets = sprite.Group()
@@ -91,19 +96,32 @@ rockets.add(rocket2)
 clock = time.Clock()
 FPS = 60
 
+first_text = font.render('Выиграл первый!', 1, (0, 0, 0))
+second_text = font.render('Выиграл второй!', 1, (0, 0, 0))
+
 game = True
+end = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
     keys_pressed = key.get_pressed()
     clock.tick(FPS)
     window.blit(background, (0,0))
-
     ball.show()
-    ball.update()
-
     rockets.draw(window)
-    rockets.update(keys_pressed)
 
+    if end == False:
+        rockets.update(keys_pressed)
+        ball.update(win_width, win_height)
+
+        sprites_list = sprite.spritecollide(ball, rockets, False)
+        for e in sprites_list:
+            ball.touch_racquet()
+    else:
+        if ball.rect.x >= (win_width - ball.width):
+            window.blit(first_text, (win_width/2, win_height/2))
+        else:
+            window.blit(second_text, (win_width/2, win_height/2))
     display.update()
